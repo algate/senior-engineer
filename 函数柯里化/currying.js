f9c8438e940c98f96dc6ebd23c159387bad8e58a
algate <361593833@qq.com>
2020-12-12 16:12:13 +0800

    🐉 新增知识点
    new Set()、防抖、节流、柯里化、数组的合集，并集，交集

--- 函数柯里化/currying.js
+++ 函数柯里化/currying.js
@@ -1,3 +1,6 @@
+sum = (a,b,c,d) => {
+    return a+b+c+d
+}
 // 初步封装
 var currying = function(fn) {
     // args 获取第一个方法内的全部参数
@@ -30,6 +33,7 @@ function progressCurrying(fn, args) {
         return fn.apply(this, _args);
     }
 }
+console.log();
 
 // 实现一个add方法，使计算结果能够满足如下预期：
 /*
@@ -41,11 +45,9 @@ add(1)(2)(3)(4)(5) = 15;
 function add() {
     // 第一次执行时，定义一个数组专门用来存储所有的参数
     var _args = Array.prototype.slice.call(arguments);
-    console.log(_args);
     // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
     var _adder = function() {
         _args.push(...arguments);
-        console.log(_args);
         return _adder;
     };
     // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回 ==== 重写toString() 方法
