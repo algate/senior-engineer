d6d902ada5f961b0915afb60ce483e6cb2213bcc
algate <361593833@qq.com>
2020-12-14 18:42:39 +0800

    🦏 增加扁平化数组、增肌高阶函数reduce、丰富函数柯里化

---
+++ Reduce用法/reduce.md
@@ -0,0 +1,56 @@
+# reduce()方法详解
+```javascript
+/* 
+-[1] callback （执行数组中每个值的函数，包含四个参数):
+	-[1.1] previousValue （上一次调用回调返回的值，或者是提供的初始值（initialValue））
+	-[1.2] currentValue （数组中当前被处理的元素）
+	-[1.3] index （当前元素在数组中的索引）
+	-[1.4] array （调用 reduce 的数组）
+-[2] initialValue （作为第一次调用 callback 的第一个参数。） 
+*/
+arr.reduce(callback,[initialValue])
+// 如下：
+arr.reduce((previousValue, currentValue, index, array) => {
+    /* 
+    -[1.1] previousValue （上一次调用回调返回的值，或者是提供的初始值（initialValue））
+	-[1.2] currentValue （数组中当前被处理的元素）
+	-[1.3] index （当前元素在数组中的索引）
+    -[1.4] array （调用 reduce 的数组）
+     */
+}, []);
+```
+
+# 使用
+
+## 数组扁平化
+```javascript
+const flattenDeep = (arr) => {
+    return Array.isArray(arr) ? arr.reduce((a, b) => {
+        return [...a, ...flattenDeep(b)];
+    }, []) : [arr];
+}
+let flattenDeepData = flattenDeep([1, [[2], [3, [4]], 5]]);
+console.log(flattenDeepData);
+```
+
+## 函数柯里化处理
+```javascript
+let sum2 = arr => {
+    return Array.isArray(arr) ? arr.reduce((a, b) => {
+        return a + b;
+    }, []): [arr];
+}
+let curry2 = (fn, ...arr) => {  // arr 记录已有参数
+    return (...args) => {  // args 接收新参数
+        if (args.length === 0) {  // 参数为空时，触发执行
+            return fn(...arr, ...args)
+        } else {  // 继续添加参数
+            return curry2(fn, ...arr, ...args)
+        }
+    }
+}
+var sumPlus = curry2(sum2)
+sumPlus(1)(2)(3)(4)()
+sumPlus(1, 2)(3)(4)()
+sumPlus(1, 2, 3)(4)()
+```
--- 函数柯里化/currying.js
+++ 函数柯里化/currying.js
@@ -1,18 +1,38 @@
-sum = (a,b,c,d) => {
-    return a+b+c+d
-}
+// 1.固定传入参数，参数够了才执行
 // 初步封装
-var currying = function(fn) {
+var currying = function (fn) {
     // args 获取第一个方法内的全部参数
     var args = Array.prototype.slice.call(arguments, 1)
-    return function() {
+    return function () {
         // 将后面方法里的全部参数和args进行合并
         var newArgs = args.concat(Array.prototype.slice.call(arguments))
         // 把合并后的参数通过apply作为fn的参数并执行
         return fn.apply(this, newArgs)
     }
 }
+// 或者
+// 1.1 待柯里化处理的函数
+let sum = (a, b, c, d) => {
+    return a + b + c + d
+}
+// 柯里化函数，返回一个被处理过的函数 
+let curry = (fn, ...arr) => { // arr 记录已有参数
+    var len = fn.length; //计算期望函数的参数长度
+    return (...args) => { // args 接收新参数
+        const combArg = [...arr, ...args];
+        if (len <= combArg.length) { // 参数够时，触发执行
+            return fn(...combArg)
+        } else { // 继续添加参数
+            return curry(fn, ...combArg)
+        }
+    }
+}
+var sumPlus = curry(sum)
+sumPlus(1)(2)(3)(4)
+sumPlus(1, 2)(3)(4)
+sumPlus(1, 2, 3)(4)
 
+// 2.不固定传入参数，随时执行
 // 支持多参数传递
 function progressCurrying(fn, args) {
 
@@ -20,7 +40,7 @@ function progressCurrying(fn, args) {
     var len = fn.length;
     var args = args || [];
 
-    return function() {
+    return function () {
         var _args = Array.prototype.slice.call(arguments);
         Array.prototype.push.apply(args, _args);
 
@@ -33,9 +53,31 @@ function progressCurrying(fn, args) {
         return fn.apply(this, _args);
     }
 }
-console.log();
+// 或者
+// 2.2 待柯里化处理的函数
+let sum2 = arr => {
+    return Array.isArray(arr) ? arr.reduce((a, b) => {
+        return a + b;
+    }, []): [arr];
+}
 
-// 实现一个add方法，使计算结果能够满足如下预期：
+let curry2 = (fn, ...arr) => {  // arr 记录已有参数
+    return (...args) => {  // args 接收新参数
+        if (args.length === 0) {  // 参数为空时，触发执行
+            return fn(...arr, ...args)
+        } else {  // 继续添加参数
+            return curry2(fn, ...arr, ...args)
+        }
+    }
+}
+
+var sumPlus = curry2(sum2)
+sumPlus(1)(2)(3)(4)()
+sumPlus(1, 2)(3)(4)()
+sumPlus(1, 2, 3)(4)()
+
+
+// 3.实现一个add方法，使计算结果能够满足如下预期：
 /*
 add(1)(2)(3) = 6;
 add(1, 2, 3)(4) = 10;
@@ -46,7 +88,7 @@ function add() {
     // 第一次执行时，定义一个数组专门用来存储所有的参数
     var _args = Array.prototype.slice.call(arguments);
     // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
-    var _adder = function() {
+    var _adder = function () {
         _args.push(...arguments);
         return _adder;
     };
@@ -59,9 +101,19 @@ function add() {
     return _adder;
 }
 
-var a = add(1)(2)(3)(4)(5)(6,7,8)                // 6
+var a = add(1)(2)(3)(4)(5)(6, 7, 8)                // 6
 // var b = add(1, 2, 3)(4)             // 10
 // var c = add(1)(2)(3)(4)(5)          // 15
 // var d = add(2, 6)(1)
 // console.info(a, b, c, d);
 console.log(a);  // 打印a的时候，隐式调用a上的toString()方法
+
+
+/* 数组扁平化 */
+const flattenDeep = (arr) => {
+    return Array.isArray(arr) ? arr.reduce((a, b) => {
+        return [...a, ...flattenDeep(b)];
+    }, []) : [arr];
+}
+let flattenDeepData = flattenDeep([1, [[2], [3, [4]], 5]]);
+console.log(flattenDeepData);
